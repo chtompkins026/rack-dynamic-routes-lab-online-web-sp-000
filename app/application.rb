ad857a0caa46b1e04259271b4d0d8e05988fc0b4
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-23 16:59:06 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -8,13 +8,13 @@ class Application
  
     if req.path.match(/items/)
       item_n = req.path.split("/items/").last 
-        
       item = @@items.find{|it| it.name == item_n} 
       
-       if item_n.nil? 
-         
-      
-      resp.write item.price 
+      if item_n.nil? 
+        resp.write "Item not found"
+        resp.status = 400
+      else 
+        resp.write item.price 
  
     resp.finish
   end
