bf4f872c85f5aff782fb4ec9243409981b210162
Christopher Tompkins <chtompkins026@gmail.com>
2018-11-23 16:56:27 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,12 +1,16 @@
 class Application
  
+ @@items = [] 
+ 
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
  
     if req.path.match(/items/)
       item_n = req.path.split("/items/").last 
-      item = @@
+      item = @@items.find{|it| it.name == item_n} 
+      
+      resp.write item.name 
  
     resp.finish
   end
